627f1aeb741fc7d276f4b1d00270d82ff179c32a
Duam Rodrigues <3drxdzin@gmail.com>
2026-04-10 10:45:14 -0300

    fase 6: painel agentes IA

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dmstack-04101039';
+const CACHE='dmstack-04101045';
 const ASSETS=['/','/index.html','/manifest.json'];
 
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
