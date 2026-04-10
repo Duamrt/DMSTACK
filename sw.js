f063a2c112702a222c16239994a008812f03737b
Duam Rodrigues <3drxdzin@gmail.com>
2026-04-10 11:12:50 -0300

    fix: triagem-ia callEdge já retorna JSON, remover r.json() duplo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dmstack-04101059';
+const CACHE='dmstack-04101112';
 const ASSETS=['/','/index.html','/manifest.json'];
 
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
