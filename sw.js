1b9a83c9df75f10ad76a450acaea72494ffc9420
Duam Rodrigues <3drxdzin@gmail.com>
2026-04-10 11:22:36 -0300

    fix: detectar erro 400 no import e mostrar feedback de sucesso/falha

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dmstack-04101119';
+const CACHE='dmstack-04101122';
 const ASSETS=['/','/index.html','/manifest.json'];
 
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
