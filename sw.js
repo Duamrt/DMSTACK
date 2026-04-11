74e3ec676e19a4224b8af379129a0625fbe551ae
Duam Rodrigues <3drxdzin@gmail.com>
2026-04-11 04:51:55 -0300

    cto score semanal: indice 0-100, 4 fatores ponderados e historico de tendencia

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dmstack-04110446';
+const CACHE='dmstack-04110451';
 const ASSETS=['/','/index.html','/manifest.json'];
 
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
