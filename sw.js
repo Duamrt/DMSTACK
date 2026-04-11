24f58fc66d825dc8addb0ac572b0e2f2435fe8cb
Duam Rodrigues <3drxdzin@gmail.com>
2026-04-11 04:41:04 -0300

    modo sprint com selecao de itens foco visual e retrospectiva

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dmstack-04110434';
+const CACHE='dmstack-04110441';
 const ASSETS=['/','/index.html','/manifest.json'];
 
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
