30ba3338ec52df4ae78300cb839b3db4a21c2fa3
Duam Rodrigues <3drxdzin@gmail.com>
2026-04-11 05:16:02 -0300

    command palette: criacao rapida e busca global

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dmstack-04110509';
+const CACHE='dmstack-04110516';
 const ASSETS=['/','/index.html','/manifest.json'];
 
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
