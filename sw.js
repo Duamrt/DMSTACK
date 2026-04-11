dd71ff61c0d5a4cd6f9341e80c7276a1f93622e1
Duam Rodrigues <3drxdzin@gmail.com>
2026-04-11 07:07:11 -0300

    feat: filtro por sistema na tela de Deploys (EDR, RPM, NaRegua, DMSTACK, LoadPro)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dmstack-04110521';
+const CACHE='dmstack-04110707';
 const ASSETS=['/','/index.html','/manifest.json'];
 
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
