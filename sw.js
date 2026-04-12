074dc2d9ae67f001eb794c703b2319eed51751fb
Duam Rodrigues <3drxdzin@gmail.com>
2026-04-12 05:42:23 -0300

    Saúde dividida em tabs: CTO Score / Financeiro / Técnico

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dmstack-04120536';
+const CACHE='dmstack-04120542';
 const ASSETS=['/','/index.html','/manifest.json'];
 
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
