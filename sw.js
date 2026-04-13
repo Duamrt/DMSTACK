6dbd8a6be0bc1858b9af0b416274656d1f85cf8a
Duam Rodrigues <3drxdzin@gmail.com>
2026-04-13 09:23:41 -0300

    feat: filtro DMSMART em bugs/demandas/deploys + badge colorido

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dmstack-04130916';
+const CACHE='dmstack-04130923';
 const ASSETS=['/','/index.html','/manifest.json'];
 
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
