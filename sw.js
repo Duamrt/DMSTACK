f38416757ce2ae62a4be60bd81eef4d1bd37c378
Duam Rodrigues <3drxdzin@gmail.com>
2026-04-13 09:16:28 -0300

    adiciona DM Smart como sistema monitorado (saude + bugs + demandas)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dmstack-04120559';
+const CACHE='dmstack-04130916';
 const ASSETS=['/','/index.html','/manifest.json'];
 
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
